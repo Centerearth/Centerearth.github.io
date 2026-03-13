9db2dbbedbd86192f8449cc26ecbb258f203c2f3
Centerearth <andrewtheamazingbrother@gmail.com>
2026-03-13 15:39:59 -0600

    Continuation

--- src/App.js
+++ src/App.js
@@ -26,22 +26,26 @@ function App() {
               <h2>FIDO2 Attacks</h2>
               <p>Implementing a basic site and server supporting FIDO2 passkey authentication so that we can later try to recreate FIDO2 attacks.</p>
               <img src="/fido2.png" alt="fido2" />
+              <p><a href="https://github.com/Centerearth/FIDO2_attacks" target="_blank" rel="noopener noreferrer">Github</a></p>
             </div>
             <div className="grid-item">
               <h2>RateMyClass</h2>
               <p>RateMyClass is a RateMyProfessor spinoff tailored for BYU classes.</p>
               <img src="/ratemyclass.png" alt="ratemyclass" />
+              <p><a href="https://github.com/Centerearth/startup" target="_blank" rel="noopener noreferrer">Github</a></p>
             </div>
             <div className="grid-item">
               <h2>Rubiks Solver</h2>
               <p>A recursive solver for the Rubik's cube.</p>
               <img src="/rubiks.png" alt="rubiks" />
+              <p><a href="https://github.com/Centerearth/rubiks_recursive" target="_blank" rel="noopener noreferrer">Github</a></p>
             </div>
           </div>
           <div className="grid-container">
             <div className="grid-item">
               <h2>Chess</h2>
               <p>Full-stack chess application.</p>
+              <p><a href="https://github.com/Centerearth/chess" target="_blank" rel="noopener noreferrer">Github</a></p>
             </div>
           </div>
         </section>
